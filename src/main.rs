02c1b4c4f7178f97481f5982d1274fd9ae502623
Ehllay <max.pruther@gmail.com>
2024-02-05 16:38:51 +0000

    Moved to libmacchina and implemented uptime

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
-use std::{env, fmt, vec};
+#[allow(unused_imports)]
+use libmacchina::{
+    traits::GeneralReadout as _, traits::KernelReadout as _, GeneralReadout, KernelReadout,
+    MemoryReadout,
+};
+
+use std::{fmt, vec};
 
 use colored::*;
 
@@ -31,30 +37,53 @@ enum Fetches {
     BIOS,
 }
 
+struct Readouts {
+    general_readout: GeneralReadout,
+    kernel_readout: KernelReadout,
+    //memory_readout: MemoryReadout,
+}
+
 impl fmt::Display for Fetches {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "{}", format!("{self:?}"))
     }
 }
 
-fn get_env(envin: &str) -> String {
-    let env = env::var(envin);
-    match env {
-        Ok(e) => e,
-        Err(_) => "Unknown".to_string(),
-    }
-}
-
-fn getinfo(info: Fetches) -> String {
+fn getinfo(info: Fetches, readout: &Readouts) -> String {
     match info {
-        Fetches::OS => whoami::distro().to_string(),
+        Fetches::OS => whoami::distro(),
         Fetches::Host => whoami::devicename(),
-        Fetches::DE => get_env("XDG_CURRENT_DESKTOP"),
-        Fetches::Kernel => todo!("Kernel not done yet"),
-        Fetches::Uptime => todo!(),
+        Fetches::DE => readout.general_readout.desktop_environment().unwrap(),
+        Fetches::Kernel => readout.kernel_readout.os_release().unwrap(),
+        Fetches::Uptime => uptime(readout),
         _ => panic!("invalid fetch type"),
     }
     .to_string()
+}
+
+//Get and format uptime
+fn uptime(readout: &Readouts) -> String {
+    let time = readout.general_readout.uptime().ok().unwrap();
+    let days = if time > 86400 {
+        (time / 86400).to_string() + "d"
+    } else {
+        "".to_string()
+    };
+    let hours = if time > 3600 {
+        (time / 3600).to_string() + "h"
+    } else {
+        "".to_string()
+    };
+    let mins = if time > 60 {
+        (time / 60).to_string() + "m"
+    } else {
+        "".to_string()
+    };
+    let secs = (time % 60).to_string() + "s";
+
+    format!("{days} {hours} {mins} {secs}")
+        .trim_start()
+        .to_owned()
 }
 
 // Fetching host and user names
@@ -66,13 +95,24 @@ fn userhost() -> [String; 3] {
     ]
 }
 
-fn printfetch(fetches: Vec<Fetches>, color: &str) {
+fn printfetch(fetches: Vec<Fetches>, color: &str, readout: &Readouts) {
     for i in fetches {
-        println!("{}{} {}", i.to_string().color(color), SEPARATOR, getinfo(i))
+        println!(
+            "{}{} {}",
+            i.to_string().color(color),
+            SEPARATOR,
+            getinfo(i, readout)
+        )
     }
 }
 
 fn main() {
+    let readouts = Readouts {
+        general_readout: GeneralReadout::new(),
+        kernel_readout: KernelReadout::new(),
+        //memory_readout: MemoryReadout::new(),
+    };
+
     let host = userhost();
     println!(
         "{}{}{}\n{}",
@@ -83,7 +123,13 @@ fn main() {
     );
 
     //Fetches
-    let fetches = vec![Fetches::OS, Fetches::Host, Fetches::DE];
+    let fetches = vec![
+        Fetches::OS,
+        Fetches::Host,
+        Fetches::DE,
+        Fetches::Kernel,
+        Fetches::Uptime,
+    ];
     //Print items
-    printfetch(fetches, "blue")
+    printfetch(fetches, "blue", &readouts)
 }
